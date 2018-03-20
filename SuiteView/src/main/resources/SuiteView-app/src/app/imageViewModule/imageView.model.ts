e32c2e67e858b75d45522ebd143468aea342b855
paulc <paulcabunilas@gmailc.om>
2018-03-20 18:23:37 +0800

    yeay

---
+++ SuiteView/src/main/resources/SuiteView-app/src/app/imageViewModule/imageView.model.ts
@@ -0,0 +1,10 @@
+export class ImageViewModel{
+    cameraName:string;
+    date:string;
+    hour:string;
+    min:string;
+    objectKey:string;
+    sec:string;
+    webUrl:string;
+    signedUrlExp:string;
+}
