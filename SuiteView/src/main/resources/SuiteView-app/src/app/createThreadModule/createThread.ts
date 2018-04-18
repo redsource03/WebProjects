4dcbabdaad3e8ec580ab007ac70a509778e34255
paulc <paulcabunilas@gmailc.om>
2018-04-18 14:10:33 +0800

    yeah yeah

--- SuiteView/src/main/resources/SuiteView-app/src/app/createThreadModule/createThread.ts
+++ SuiteView/src/main/resources/SuiteView-app/src/app/createThreadModule/createThread.ts
@@ -1,7 +1,7 @@
 import { Component,Input } from '@angular/core';
 import { ThreadService } from '../service/thread.service';
 import {  BlockUI, NgBlockUI } from 'ng-block-ui';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal, ModalDismissReasons,NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
 @Component({
     selector: 'createThread',
   templateUrl: './createThread.html',
@@ -12,22 +12,24 @@ export class CreateThreadComponent {
     body:string;
     @Input() user:any;
     @BlockUI() blockUI: NgBlockUI;
-    constructor(private threadService: ThreadService,private modalService: NgbModal) {}
+    private modalRef: NgbModalRef;
+    constructor(private threadService: ThreadService,
+      private modalService: NgbModal,
+    ) {}
     open(content) {
-        this.modalService.open(content,{ size: 'lg' }).result.then((result) => {
-      
-        }, (reason) => {
-          
-        });
+       this.modalRef= this.modalService.open(content,{ size: 'lg' });
       }
     createThread(){
+      this.modalRef.close();
       this.blockUI.start("Sending Message");
       this.threadService.createThread(this.subject,this.body).subscribe(
         (model: any) => {
           this.blockUI.stop();
           this.blockUI.start("Message Sent");
-   
-          setTimeout(() => {
+        
+          setTimeout(() => { 
+            this.subject="";
+            this.body="";
             this.blockUI.stop();
           }, 2000);
         },
